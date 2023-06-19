8838e372ebad9f07fc327223eef689f78d4c2aaa
Bernard Cardoso <be1.0@hotmail.com>
2023-06-19 23:37:54 +0100

    More updates

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,57 @@
+const axios = require('axios');
+const APIHandler = require('./APIHandler');
+
+const api = new APIHandler('http://localhost:8000');
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList () {
+    try {
+      const response = await axios.get(`${this.BASE_URL}/characters`);
+      return response.data;
+    } catch (error) {
+      console.error("Error retrieving all characters: ", error);
+    }
   }
 
-  getOneRegister () {
-
+  async getOneRegister (id) {
+    try {
+      const response = await axios.get(`${this.BASE_URL}/characters/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error retrieving character Id: ", error);
+    }
   }
 
-  createOneRegister () {
-
+  async createOneRegister(characterData) {
+    try {
+      const response = await axios.post(`${this.BASE_URL}/characters`, characterData);
+      return response.data;
+    } catch (error) {
+      console.error("Error creating character: ", error);
+    }
   }
 
-  updateOneRegister () {
-
+  async updateOneRegister(id, characterData) {
+    try {
+      const response = await axios.put(`${this.BASE_URL}/characters/${id}`, characterData);
+      return response.data;
+    } catch (error) {
+      console.error(`Error updating character with ID ${id}: `, error);
+    }
   }
 
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    try {
+      const response = await axios.delete(`${this.BASE_URL}/characters/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Error deleting character with ID ${id}: `, error);
+    }
   }
 }
+
+module.exports = APIHandler;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,14 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+window.addEventListener('load', () => {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    try {
+      const characters = await charactersAPI.getFullList();
+      showCharacters(characters);
+    } catch (error) {
+      console.error('Error retrieving characters:', error);
+    }
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
@@ -21,3 +27,44 @@ window.addEventListener('load', () => {
 
   });
 });
+
+function showCharacters(characters) {
+  const charactersContainer = document.getElementById('characters-container');
+  charactersContainer.innerHTML = '';
+
+  characters.forEach(character => {
+    const characterCard = createCharacterCard(character);
+    charactersContainer.appendChild(characterCard);
+  });
+}
+
+function createCharacterCard(character) {
+  const characterCard = document.createElement('div');
+  characterCard.classList.add('character-card');
+
+  const characterInfo = document.createElement('div');
+  characterInfo.classList.add('character-info');
+  characterCard.appendChild(characterInfo);
+
+  const name = document.createElement('div');
+  name.classList.add('name');
+  name.textContent = character.name;
+  characterInfo.appendChild(name);
+
+  const occupation = document.createElement('div');
+  occupation.classList.add('occupation');
+  occupation.textContent = character.occupation;
+  characterInfo.appendChild(occupation);
+
+  const cartoon = document.createElement('div');
+  cartoon.classList.add('cartoon');
+  cartoon.textContent = character.cartoon ? 'Is a Cartoon' : 'Is not a Cartoon';
+  characterInfo.appendChild(cartoon);
+
+  const weapon = document.createElement('div');
+  weapon.classList.add('weapon');
+  weapon.textContent = character.weapon;
+  characterInfo.appendChild(weapon);
+
+  return characterCard;
+}
