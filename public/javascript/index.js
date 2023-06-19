5a7aeca0c6daf1fcbfb96f2bf2eaf5aed3623e5b
Bernard Cardoso <be1.0@hotmail.com>
2023-06-19 23:55:44 +0100

    Done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -7,24 +7,66 @@ window.addEventListener('load', () => {
       const characters = await charactersAPI.getFullList();
       showCharacters(characters);
     } catch (error) {
-      console.error('Error retrieving characters:', error);
+      console.error('Error retrieving characters: ', error);
     }
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    try {
+      const characterId = document.getElementById('character-id').value;
+      const character = await charactersAPI.getOneRegister(characterId);
+      showCharacter(character);
+    } catch (error) {
+      console.error('Error retrieving character:', error);
+    }
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    try {
+      const characterId = document.getElementById('character-id').value;
+      await charactersAPI.deleteOneRegister(characterId);
+      setButtonBackgroundColor('delete-one', 'green');
+    } catch (error) {
+      console.error('Error deleting character:', error);
+      setButtonBackgroundColor('delete-one', 'red');
+    }
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
+    try {
+      const form = event.target;
+      const characterId = form.elements.id.value;
+      const characterData = {
+        name: form.elements.name.value,
+        occupation: form.elements.occupation.value,
+        weapon: form.elements.weapon.value,
+        cartoon: form.elements.cartoon.checked
+      };
+  
+      await charactersAPI.updateOneRegister(characterId, characterData);
+      setButtonBackgroundColor('update-button', 'green');
+    } catch (error) {
+      console.error('Error updating character:', error);
+      setButtonBackgroundColor('update-button', 'red');
+    }
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    try {
+      const form = event.target;
+      const characterData = {
+        name: form.elements.name.value,
+        occupation: form.elements.occupation.value,
+        weapon: form.elements.weapon.value,
+        cartoon: form.elements.cartoon.checked
+      };
+  
+      await charactersAPI.createOneRegister(characterData);
+      setButtonBackgroundColor('submit-button', 'green');
+    } catch (error) {
+      console.error('Error creating character:', error);
+      setButtonBackgroundColor('submit-button', 'red');
+    }
   });
 });
 
@@ -36,7 +78,7 @@ function showCharacters(characters) {
     const characterCard = createCharacterCard(character);
     charactersContainer.appendChild(characterCard);
   });
-}
+};
 
 function createCharacterCard(character) {
   const characterCard = document.createElement('div');
@@ -67,4 +109,27 @@ function createCharacterCard(character) {
   characterInfo.appendChild(weapon);
 
   return characterCard;
-}
+};
+
+function showCharacter(character) {
+  const characterContainer = document.getElementById('character-container');
+  characterContainer.innerHTML = '';
+
+  const characterCard = createCharacterCard(character);
+  characterContainer.appendChild(characterCard);
+};
+
+function setButtonBackgroundColor(buttonId, color) {
+  const button = document.getElementById(buttonId);
+  button.style.backgroundColor = color;
+};
+
+function setButtonBackgroundColor(buttonId, color) {
+  const button = document.getElementById(buttonId);
+  button.style.backgroundColor = color;
+};
+
+function setButtonBackgroundColor(buttonId, color) {
+  const button = document.getElementById(buttonId);
+  button.style.backgroundColor = color;
+};
